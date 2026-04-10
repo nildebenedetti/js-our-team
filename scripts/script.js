ba93c971b223074a23fcb319a355f831a1aa8636
“Nilde <nilde.benedetti@gmail.com>
2026-04-10 16:20:48 +0200

    add:macrostrura programma

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,11 @@
-console.log('ciao mamma');
+'user strict';
+
+// per ogni oggetto, voglio creare la mia card 
+// 
+
+for (const teamMember of teamMembers) {
+
+    // creami un div card fatto in questo modo
+
+    //inseriscilo in container
+}
