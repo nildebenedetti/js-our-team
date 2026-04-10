49c9462efb06c33fdfbca45da315d885786c173d
“Nilde <nilde.benedetti@gmail.com>
2026-04-10 17:51:24 +0200

    fix: rimossa struttura row cols, sistemate card con css a manina

--- scripts/script.js
+++ scripts/script.js
@@ -11,10 +11,10 @@ let tempContentContainer = '';
 for (const teamMember of teamMembers) {
 
    tempContentContainer += `<div class="card row bg-light m-3">
-                    <div class="image-container col-4 ">
+                    <div class="image-container">
                         <img src="${teamMember.img}" alt="${teamMember.name}" class="portrait">
                     </div>
-                    <div class="member-info p-4 col-8">
+                    <div class="member-info px-2 py-3">
                         <h3 class="name-surname py-2 fw-bold">${teamMember.name}</h3>
                         <h5 class="role py-1">${teamMember.role}</h4>
                         <a href="mailto:${teamMember.email}" class="email pt-2 text-info">${teamMember.email}</a>
