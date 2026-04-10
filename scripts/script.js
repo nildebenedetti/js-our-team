79c11be51ef76efdabbcc6ccf25803f5d993a6ec
“Nilde <nilde.benedetti@gmail.com>
2026-04-10 16:39:57 +0200

    add: creo l'html di tutte le card con il ciclo for of

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,24 @@
 'user strict';
 
 // per ogni oggetto, voglio creare la mia card 
-// 
+
+// identifico le variabili che mi servono nel dom
+
+const mainContainer = document.querySelector('.page-main');
+
 
 for (const teamMember of teamMembers) {
 
-    // creami un div card fatto in questo modo
+   const currentCard = `<div class="card container d-inline-flex flex-row bg-light">
+                    <div class="image-container">
+                        <img src="${teamMember.img}" alt="${teamMember.name}" class="portrait img-fluid">
+                    </div>
+                    <div class="member-info p-4 flex-grow-1">
+                        <h3 class="name-surname py-2 fw-bold">${teamMember.name}</h3>
+                        <h5 class="role py-1">Designer</h4>
+                        <a href="mailto:${teamMember.email}" class="email pt-2 text-info">${teamMember.email}</a>
+                    </div>
+            </div>`;
+            console.log(currentCard)
 
-    //inseriscilo in container
 }
